57c71e1f399801df040af26f0a13b6abbbadbc39
Marko Sladojevic <mrsladoje@gmail.com>
2026-01-21 13:19:08 +0100

    feature: add eslint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,56 @@
+// @ts-check
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import angularPlugin from "@angular-eslint/eslint-plugin";
+import angularTemplatePlugin from "@angular-eslint/eslint-plugin-template";
+import angularTemplateParser from "@angular-eslint/template-parser";
+
+export default tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+    ],
+    plugins: {
+      "@angular-eslint": angularPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    languageOptions: {
+      parser: angularTemplateParser,
+    },
+    plugins: {
+      "@angular-eslint/template": angularTemplatePlugin,
+    },
+    rules: {
+      "@angular-eslint/template/button-has-type": "error",
+      "@angular-eslint/template/no-duplicate-attributes": "error",
+    },
+  }
+);
