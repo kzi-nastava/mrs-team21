79df81524be9a28850ab9cfadb5055eefe56bf76
Milos Jovanovic <milosjovanovic519@gmail.com>
2026-02-11 17:14:04 +0100

    feat: configure server settings and update library versions

--- drumigo/src/main/java/com/ftn/drumigo/config/WebConfig.java
+++ drumigo/src/main/java/com/ftn/drumigo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "http://192.168.1.6:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
