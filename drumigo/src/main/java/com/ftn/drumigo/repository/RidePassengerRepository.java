f9f7133dcffb0253d0343bdfdf0e53325c9d698f
Milos Jovanovic <milosjovanovic519@gmail.com>
2026-02-12 02:16:10 +0100

    refactor: remove unused query method from RidePassengerRepository

--- drumigo/src/main/java/com/ftn/drumigo/repository/RidePassengerRepository.java
+++ drumigo/src/main/java/com/ftn/drumigo/repository/RidePassengerRepository.java
@@ -3,8 +3,6 @@ package com.ftn.drumigo.repository;
 import com.ftn.drumigo.domain.Ride;
 import com.ftn.drumigo.domain.RidePassenger;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,7 +12,5 @@ public interface RidePassengerRepository extends JpaRepository<RidePassenger, Ri
     List<RidePassenger> findByRide(Ride ride);
 
     boolean existsByRideAndPassengerEmail(Ride ride, String passengerEmail);
-
-    List<RidePassenger> findByPassengerEmail(String passengerEmail);
 }
 
