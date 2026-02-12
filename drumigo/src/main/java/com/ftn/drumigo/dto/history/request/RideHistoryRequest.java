f740af9bbb238337e227d8e5f7e938767d626d7c
Milos Jovanovic <milosjovanovic519@gmail.com>
2026-02-12 02:16:19 +0100

    refactor: simplify RideHistoryRequest by using Lombok annotations

--- drumigo/src/main/java/com/ftn/drumigo/dto/history/request/RideHistoryRequest.java
+++ drumigo/src/main/java/com/ftn/drumigo/dto/history/request/RideHistoryRequest.java
@@ -2,6 +2,10 @@ package com.ftn.drumigo.dto.history.request;
 
 import com.ftn.drumigo.domain.enums.RideStatus;
 import com.ftn.drumigo.exception.BadRequestException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -11,6 +15,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RideHistoryRequest {
 
     private Instant from;
@@ -21,74 +29,6 @@ public class RideHistoryRequest {
     private int size = 10;
     private String sort = "requestedAt,desc";
 
-    public RideHistoryRequest() {}
-
-    public RideHistoryRequest(Instant from, Instant to, String status, Boolean hasPanic,
-                             int page, int size, String sort) {
-        this.from = from;
-        this.to = to;
-        this.status = status;
-        this.hasPanic = hasPanic;
-        this.page = page;
-        this.size = size;
-        this.sort = sort;
-    }
-
-    public Instant getFrom() {
-        return from;
-    }
-
-    public void setFrom(Instant from) {
-        this.from = from;
-    }
-
-    public Instant getTo() {
-        return to;
-    }
-
-    public void setTo(Instant to) {
-        this.to = to;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Boolean getHasPanic() {
-        return hasPanic;
-    }
-
-    public void setHasPanic(Boolean hasPanic) {
-        this.hasPanic = hasPanic;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public String getSort() {
-        return sort;
-    }
-
-    public void setSort(String sort) {
-        this.sort = sort;
-    }
 
     /**
      * Parse the comma-separated status string into a list of RideStatus enums.
