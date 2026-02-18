9a889aea1aacba507d439fe37f31ed6cd11047cd
Vukan Radojevic <vukanradojevicc@gmail.com>
2026-02-18 17:44:21 +0100

    feat(backend): link FavoriteRoute to source ride for history favorites

--- drumigo/src/main/java/com/ftn/drumigo/repository/FavoriteRouteRepository.java
+++ drumigo/src/main/java/com/ftn/drumigo/repository/FavoriteRouteRepository.java
@@ -5,10 +5,16 @@ import com.ftn.drumigo.domain.users.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FavoriteRouteRepository extends JpaRepository<FavoriteRoute, Long> {
     List<FavoriteRoute> findByPassenger(Passenger passenger);
+
+    Optional<FavoriteRoute> findByPassengerAndSourceRideId(Passenger passenger, Long sourceRideId);
+
+    List<FavoriteRoute> findByPassengerAndSourceRideIdIn(Passenger passenger, Collection<Long> sourceRideIds);
 }
 
